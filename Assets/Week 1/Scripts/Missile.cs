6ca453fa4a947370751e0f5f99f575d439fb9347
Lorraine Slakinski <slakinsa@shernet.sheridancollege.ca>
2024-01-18 14:20:09 -0500

    Task 1 missile collision

--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -6,16 +6,30 @@ using UnityEngine;
 public class Missile : MonoBehaviour
 {
     // Variables
-    float speed = 0.1f;
+    //float speed = 0.1f;
+    Rigidbody2D rigidbody;
+    Vector2 direction = new Vector2(10, 0);
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(speed, 0, 0);
+        //transform.Translate(speed, 0, 0);
+    }
+
+    // every 0.2s
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + direction * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Missile Hit");
+        Destroy(gameObject);
     }
 }
