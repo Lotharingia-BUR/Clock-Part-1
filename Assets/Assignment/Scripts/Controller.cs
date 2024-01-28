4c8cfe36f5fba2834ac7fe61a92c89f6479fb7af
Lorraine <lorraine@sdf1.net>
2024-01-28 16:17:54 -0500

    Move Forward with S Key

--- Assets/Assignment/Scripts/Controller.cs
+++ Assets/Assignment/Scripts/Controller.cs
@@ -5,24 +5,29 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     Rigidbody2D rigidbody;
+    public Transform controller;
     float thrustInput;
-    public float speed = 10f;
+    public float speed = 100f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float thruster = Input.GetAxis("Vertical");
-
-        rigidbody.Translate(0, 0, 1);
+        
+        Vector3 mov = transform.up * thrustInput * speed * Time.deltaTime;
+        transform.Translate(mov);
     }
 
     void FixedUpdate()
     {
-
+        thrustInput = Input.GetAxis("Vertical");
+        if (thrustInput > 0)
+        {
+            thrustInput = 0;
+        }
     }
 }
